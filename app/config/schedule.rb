ee247206f80e45fe515cc6f5166ae6c85f23e05f
Krishnan Mani <km@krishnanm.com>
2017-08-11 01:08:20 +0530

    Reading region from env var

--- app/config/schedule.rb
+++ app/config/schedule.rb
@@ -1,6 +1,6 @@
 # Use this file to easily define all of your cron jobs.
 
 every 60.seconds do
-  region = ENV['AWS_REGION'] || 'us-east-1'
+  region = ENV['AWS_REGION'] or raise 'Missing AWS region'
   rake "write_item[\"#{region}\"]"
 end
