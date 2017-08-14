51455cf08d90a2f19d48be25c07487721177d817
Krishnan Mani <km@krishnanm.com>
2017-08-14 18:48:43 +0530

    Improved error handling

--- scripting/put_items_every_minute.sh
+++ scripting/put_items_every_minute.sh
@@ -3,13 +3,14 @@
 set -euo pipefail
 
 SCRIPT_NAME="$0"
-REGION="$1"
 
 if [ $# -ne 1 ]
 then
+    echo "Error: missing argument"
     echo "Usage: ${SCRIPT_NAME} aws-region"
     exit 1
 fi
+REGION="$1"
 
 table_name="items_every_minute"
 HOSTNAME=$(hostname)
