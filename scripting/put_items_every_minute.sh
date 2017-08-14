de0467bab6b898108b64c7da9afbe6db72938883
Krishnan Mani <km@krishnanm.com>
2017-08-14 18:33:23 +0530

    Handled table exists scenario

--- scripting/put_items_every_minute.sh
+++ scripting/put_items_every_minute.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 SCRIPT_NAME="$0"
 REGION="$1"
@@ -11,24 +11,37 @@ then
     exit 1
 fi
 
-table_name="test_items_every_minute"
+table_name="items_every_minute"
+HOSTNAME=$(hostname)
 
-# create table
-aws --region ${REGION} dynamodb create-table \
+DESCRIBE_TABLE=$(aws dynamodb describe-table \
     --table-name ${table_name} \
-    --cli-input-json file://table_definition.json
+    --query "Table.[TableName]" --output text || hostname )
 
-sleep 30
+if [ "$DESCRIBE_TABLE" = "$table_name" ]
+then
+    echo "Table exists: ${DESCRIBE_TABLE}"
 
-# update TTL
-aws --region ${REGION} dynamodb update-time-to-live \
-    --table-name ${table_name} \
-    --time-to-live-specification Enabled=true,AttributeName=expireAt
+else
+    # create table
+
+    echo "Creating table: ${table_name}"
+    aws --region ${REGION} dynamodb create-table \
+        --table-name ${table_name} \
+        --cli-input-json file://table_definition.json
+
+    sleep 30
+
+    # update TTL
+    aws --region ${REGION} dynamodb update-time-to-live \
+        --table-name ${table_name} \
+        --time-to-live-specification Enabled=true,AttributeName=expireAt
+
+fi
 
 NOW_IN_MINUTES=$(date +"%Y%m%dT%H%M%z")
 EPOCH_TIME=$(date +%s)
 ONE_DAY_LATER=$(($EPOCH_TIME+86400))
-HOSTNAME=$(hostname)
 
 item="{
     \"id\": {\"S\":\"${NOW_IN_MINUTES}\"},
