ea869077a238f0502eb689989abcf7c0b8d9f83c
Krishnan Mani <km@krishnanm.com>
2017-08-14 18:48:55 +0530

    Added script to generate crontab entry

---
+++ scripting/generate_cron_entry.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_NAME="$0"
+if [ $# -ne 1 ]
+then
+    echo "Error: missing argument"
+    echo "Usage: ${SCRIPT_NAME} aws-region"
+    exit 1
+fi
+
+REGION="$1"
+
+crontab -l > /tmp/mycron
+echo "* * * * * /home/ec2-user/put_items_every_minute.sh ${REGION}" > /tmp/mycron
+crontab /tmp/mycron
